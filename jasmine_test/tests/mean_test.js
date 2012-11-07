7e75187e78c63582b712a36f10a12e4131fa1525
Doug McInnes <dmcinnes@attinteractive.com>
2012-11-06 17:37:07 -0800

    make mean test less crazy number

--- jasmine_test/tests/mean_test.js
+++ jasmine_test/tests/mean_test.js
@@ -4,7 +4,7 @@ describe("mean.js", function () {
 
   describe("Math.prototype.mean", function () {
     it("returns the average for an array of numbers", function () {
-      expect(Math.mean([1,2,3,4,5,10])).toEqual(4.166666666666667);
+      expect(Math.mean([1,2,3,4,5,12])).toEqual(4.5);
     });
 
     it("returns 0 for empty arrays", function () {
